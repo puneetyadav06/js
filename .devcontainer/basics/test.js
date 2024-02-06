9d19c68f000bb381d845ef659ed009f4463746e9
puneet49 <78782364+puneet49@users.noreply.github.com>
2024-02-06 10:10:21 +0000

    container config done

--- .devcontainer/basics/test.js
+++ .devcontainer/basics/test.js
@@ -1,1 +1,8 @@
-console.log("puneet")
+
+const accountId="201599020"
+let accountEmail="puneet65yadav@gmail.com"
+var accountPassword="12345"
+accountCity="mathura"
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
