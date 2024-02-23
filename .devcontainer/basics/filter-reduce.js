11f7b67c1f001622fc38a3f0c93066f0d72cef38
Puneet Yadav <78782364+puneetyadav06@users.noreply.github.com>
2024-02-23 09:42:29 +0000

    container done

--- .devcontainer/basics/filter-reduce.js
+++ .devcontainer/basics/filter-reduce.js
@@ -1,4 +1,4 @@
-const mynumber=[1,2,3,4,5,6,7,8,9,10]
+// const mynumber=[1,2,3,4,5,6,7,8,9,10]
 // const my= mynumber.filter((mynumber) => mynumber>5) // first method without object single function
 
 // const my=mynumber.filter((mynumber)=>{  // if you open a arrow function scope then {} the you use return
@@ -21,6 +21,80 @@ const books = [
 
 // let userbooks=books.filter((books) => books.genre === 'History')
 let userbooksone=books.filter((books) =>{
-    return books.publish >= 2000;
+    return books.publish >= 1995 && books.genre === 'History';
 })
-console.log(userbooksone);
+// console.log(userbooksone);
+
+// ================================================
+// const mynum=[1,2,3,4,5,6,7,8,9,10]
+// const num=mynum.map((mynum)=> mynum+10)
+// console.log(num);
+
+// ==========scope========
+
+// const mynum=[1,2,3,4,5,6,7,8,9,10]
+// const num=mynum.map((mynum)=> {
+//   return mynum+10
+// })
+// console.log(num);
+
+// ============without scope=============
+
+// const mynum=[1,2,3,4,5,6,7,8,9,10]
+// const numb=mynum.map((mynum)=> mynum*10+5)
+// console.log(numb);
+
+//==========with scope===========
+// const mynum=[1,2,3,4,5,6,7,8,9,10]
+// const numb=mynum.map((mynum)=> {
+//   return mynum*10+5
+// })
+// console.log(numb);
+
+// ===================chaining=======
+//  const mynum=[1,2,3,4,5,6,7,8,9,10]
+// const numb=mynum
+// .map((mynum)=> mynum*10)
+// .map((mynum)=> mynum + 5)
+// console.log(numb);
+// ===================chaining include other method============
+// const mynum=[1,2,3,4,5,6,7,8,9,10]
+// const numb=mynum
+// .map((mynum)=> mynum*10)
+// .map((mynum)=> mynum + 5)
+// .filter((mynum)=> mynum>20)
+// console.log(numb);
+
+// =========reduce=========
+// const numb=[1,2,3]
+// const total=numb.reduce(function(acc,currval){
+//   console.log(`acc:${acc} and currval: ${currval}`);
+//   return acc+currval
+// },3)
+// console.log(total);
+
+// =========reduce arrow=========
+// const numb=[1,2,3]
+// const total=numb.reduce((acc,currval)=>{
+//   console.log(`acc:${acc} and currval: ${currval}`);
+//   return acc+currval
+// },3)
+// console.log(total);
+
+const shoppingcart=[
+  {
+    itemname:"mobile dev course",
+    price:12000
+  },
+  {
+    itemname:"data science course",
+    price:18000
+  },
+  {
+    itemname:"software development course",
+    price:21000
+  }
+]
+
+const pricetopay=shoppingcart.reduce((acc,item)=> acc + item.price,0)
+console.log(pricetopay);
